a41d1dffdf14bbe5ef37c6f1c1f3b047587eb6b2
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-07-01 10:04:26 -0700

    working on plants routes

--- app/api/comments_routes.py
+++ app/api/comments_routes.py
@@ -5,3 +5,56 @@ from app.models.db import db
 from app.forms.comment_form import CommentForm
 
 comments_routes = Blueprint('comments', __name__)
+
+
+# GET /api/comments
+@comments_routes.route('/')
+def all_comments():
+    comments = Comment.query.all()
+    return {"comments": [comment.to_dict() for comment in comments]}
+
+
+# POST /api/comments
+@comments_routes.route('/new_comment', methods=["POST"])
+@login_required
+def create_comment():
+    form = CommentForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    comment = Comment()
+    if form.validate():
+        form.populate_obj(comment)
+
+    db.session.add(comment)
+    db.session.commit()
+
+    return comment.to_dict()
+
+
+# PUT /api/comments/commentId
+@comments_routes.route('/<int:commentId>', methods=["PUT"])
+@login_required
+def update_comment(commentId):
+    comment = Comment.query.get(commentId)
+
+    form = CommentForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    form['userId'].data = comment.userId
+    form['plantId'].data = comment.plantId
+
+    if form.validate():
+        form.populate_obj(comment)
+        db.session.commit()
+
+    return comment.to_dict()
+
+
+# DELETE /api/comments/commentId
+@comments_routes.route('/<int:commentId>', methods=["DELETE"])
+@login_required
+def delete_comment(commentId):
+    comment = Comment.query.get(commentId)
+    db.session.delete(comment)
+    db.session.commit()
+
+    return jsonify("Success")
--- app/api/plants_routes.py
+++ app/api/plants_routes.py
@@ -3,5 +3,21 @@ from flask_login import login_required, current_user
 from app.models import Plant
 from app.models.db import db
 from app.forms.plant_form import PlantForm
+from app.s3_helpers import (upload_file_to_s3, allowed_file, get_unique_filename)
 
 plants_routes = Blueprint('plants', __name__)
+
+# GET /api/plants
+@plants_routes.route('/')
+def all_plants():
+    plants = Plant.query.all()
+    return {"plants": [plant.to_dict() for plant in plants]}
+
+# GET /api/plants/variety
+# GET /api/plants/light
+# GET /api/plants/size
+# GET /api/plants/difficulty
+# GET /api/plants/plantId
+# POST /api/plants
+# PUT /api/plants/plantId
+# DELETE /api/plants/plantId
