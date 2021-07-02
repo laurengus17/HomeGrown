ea0ab234ef0a565e2c0706095e7c1b7483871cc6
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-07-02 14:21:42 -0700

    replace

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
 import os
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_ECHO = True
