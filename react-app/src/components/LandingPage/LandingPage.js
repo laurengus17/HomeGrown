0f7b1bfcd1aee9a0ca2a650b7775cc051684421b
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-07-03 18:42:19 -0700

    styling

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import splash from '../../images/splash.png';
+import Welcome from '../../images/Welcome.png'
 import './LandingPage.css'
 
 const LandingPage = () => {
@@ -10,10 +12,15 @@ const LandingPage = () => {
     }
 
     return (
-        <>
-            <h2 className='welcome_message'>Welcome to Home Grown</h2>
-            <button className='button_enter' onClick={enterSite}>grow with us</button>
-        </>
+        <div className='container_landing'>
+        <img src={splash} alt='monstera' className='landing_background' />
+        <div className='landing_div'>
+            <img src={Welcome} alt='welcome' className='landing_message' />
+                <div className='button_div'>
+                    <button className='button_enter' onClick={enterSite}>grow with us</button>
+                </div>
+        </div>
+        </div>
     )
 }
 
